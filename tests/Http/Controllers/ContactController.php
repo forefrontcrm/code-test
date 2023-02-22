1c871da49ddf9a59967ca123130825b88f6511e2
Shawn Pivonka <shawnpivonka@Shawns-MBP.attlocal.net>
2023-02-22 10:14:38 -0600

    added more data so testing is not too bad

--- tests/Http/Controllers/ContactController.php
+++ tests/Http/Controllers/ContactController.php
@@ -84,20 +84,48 @@ class ContactController extends \Tests\TestCase
 
     /**
      * @test
+     * @dataProvider filteredRequestDataProvider
      */
-    public function getFilteredContactList()
+    public function getFilteredContactList($request, $expectedResults)
     {
-        /**
-         * Filter By Age: by greater then, less then, between, or all ages
-         * Filter By Type: is, is not, or all
-         *          Note:  We may add more types in the future.
-         */
-        $request = [
+        $contact = new Contact([
+            'first_name' => 'Willy',
+            'last_name' => 'Wonka',
+            'age' => 36,
+            'type' => 2, // 1: Seller, 2: Buyer
+        ]);
+        $contact->secret = Str::uuid();
+        $contact->save();
 
-        ];
-
-        $response = $this->postJson("/api/contacts", $request);
+        $response = $this->getJson("/api/contacts?" . http_build_query($request));
         $response->assertOk();
+        $response->assertJsonCount($expectedResults, 'data');
+    }
+
+    private function filteredRequestDataProvider(): array
+    {
+
+        /**
+         *      Filter By Age: by greater then, less then, between, or all ages
+         *      Filter By Type: is, is not, or all
+         *              Note:  We may add more types in the future.
+         *
+         *  'age_filter': '>', '<', 'between', 'all'
+         *  'type_filter': '=', '!=', 'all'
+         */
+        return [
+            [['age_filter' => '<', 'age_value' => 30,             'type_filter' => 'all', 'type_value' => null], 0],
+            [['age_filter' => '=', 'age_value' => 36,             'type_filter' => 'all', 'type_value' => null], 1],
+            [['age_filter' => '>', 'age_value' => 30,             'type_filter' => 'all', 'type_value' => null], 1],
+            [['age_filter' => 'between', 'age_value' => [30, 60], 'type_filter' => 'all', 'type_value' => null], 1],
+            [['age_filter' => 'between', 'age_value' => [50, 60], 'type_filter' => 'all', 'type_value' => null], 0],
+            [['age_filter' => 'all', 'age_value' => null,         'type_filter' => 'all', 'type_value' => null], 1],
+
+
+            [['age_filter' => 'all', 'age_value' => null, 'type_filter' => '=',   'type_value' => 2],    1],
+            [['age_filter' => 'all', 'age_value' => null, 'type_filter' => '!=',  'type_value' => 2],    0],
+            [['age_filter' => 'all', 'age_value' => null, 'type_filter' => 'all', 'type_value' => null], 1],
+        ];
     }
 }
 
